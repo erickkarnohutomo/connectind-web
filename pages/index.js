b7be9f301a8928659ffe2504f8641dcffdbbc386
erickkarnohutomo <86392708+erickkarnohutomo@users.noreply.github.com>
2025-07-21 15:56:30 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Selamat Datang di CONNECT.IND</h1>
+      <p>Web berhasil dideploy 🎉</p>
+    </div>
+  );
+}
+// Halaman login utama
